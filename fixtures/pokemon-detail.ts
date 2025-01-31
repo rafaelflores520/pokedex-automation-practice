b8dde59ec8e27f0ed6eeed5974fcee5cbacd036e
Rafael Flores <rflores@teampurpose.com>
2025-01-30 22:58:06 -0600

    Missing async on NationalId test

--- fixtures/pokemon-detail.ts
+++ fixtures/pokemon-detail.ts
@@ -1,49 +1,55 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
+import { MinimalPage } from "../Interfaces/page";
 
-export class PokemonDetails {
-  private readonly _pokeTitle: string;
-  private readonly _pokeImage: RegExp;
-  private readonly _pokeType: string;
-  private readonly _pokeName: string;
+export class PokemonDetails implements MinimalPage {
+  constructor(private readonly page: Page) {}
 
-  constructor(readonly page: Page, pokeInfo: Pokemon) {
-    this._pokeName = pokeInfo.name;
-    this._pokeImage = new RegExp(
-      String.raw`${pokeInfo.name}.+Ken Sugimori`,
-      "g"
-    );
-    this._pokeType = `Type ${pokeInfo.type
-      .map((elem) => {
-        return elem.name;
-      })
-      .join(" ")}`;
-  }
-
-  get pokeNationalId() {
-    return this.page
-      .getByRole("row", { name: /National/ })
-      .getByRole("cell", { name: /\d+/ });
-  }
-
-  get findPokeTitle() {
-    return this.page.getByRole("heading", {
-      name: this._pokeName,
-      exact: true,
+  public async goTo(path: string) {
+    await this.page.goto(`pokedex/${path.toLocaleLowerCase()}`, {
+      waitUntil: "domcontentloaded",
     });
   }
 
-  get findPokeType() {
-    return this.page.getByRole("row", { name: this._pokeType });
+  public async expectNationalIdToMatch(pokeId: number) {
+    const fetchedId = await this.page
+      .getByRole("row", { name: /National/ })
+      .getByRole("cell", { name: /\d+/ });
+
+    await expect(fetchedId).toContainText(pokeId.toString());
   }
 
-  get findPokeImage() {
-    return this.page.getByRole("img", { name: this._pokeImage });
+  public async expectPokeTitleDisplayed(pokeName: string) {
+    const fetchedName = await this.page.getByRole("heading", {
+      name: pokeName,
+      exact: true,
+    });
+    await expect(fetchedName).toBeVisible();
   }
 
-  get pokeAbilities() {
-    return this.page
+  public async expectPokeTypeToMatch(pokeTypes: Array<Type>) {
+    const fetchedTypes = await this.page
+      .getByRole("row", { name: /Type/ })
+      .getByRole("link")
+      .all();
+    for (let i = 0; i < fetchedTypes.length; i++) {
+      await expect(fetchedTypes[i]).toContainText(pokeTypes[i].name);
+    }
+  }
+
+  public async expectPokeImageDisplayed(pokeName: string) {
+    const fetchedImage = await this.page.getByRole("img", {
+      name: new RegExp(String.raw`${pokeName}.+Ken Sugimori`, "g"),
+    });
+    await expect(fetchedImage).toBeVisible();
+  }
+
+  public async expectPokeAbilitiesToMatch(pokeAbilities) {
+    const fetchedAbilities = await this.page
       .getByRole("row", { name: /Abilities/ })
       .locator(".text-muted")
       .all();
+    for (let i = 0; i < fetchedAbilities.length; i++) {
+      await expect(fetchedAbilities[i]).toContainText(pokeAbilities[i].name);
+    }
   }
 }
